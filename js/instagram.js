b1a0644224b610f048aa7d83bd7579892cc6273f
Hoc N <hocnv.qn@gmail.com>
2016-10-02 21:52:21 +0900

    first commit

---
+++ js/instagram.js
@@ -0,0 +1,70 @@
+Vue.config.delimiters = ['{', '}'];
+
+var instagramPhoto = Vue.extend({
+  props: ['photos', 'id'],
+
+  template: '<a href="http://www.instagram.com/jaswighq" :style.sync="getBackgroundImage">{photo}</a>',
+
+  data: function () {
+    return {
+      tmpPhotos: this.photos
+    }
+  },
+
+  computed: {
+    getBackgroundImage: function () {
+      return "background-image: url('"+ this.tmpPhotos[this.id] +"')"
+    }
+  }
+});
+
+var instagram = new Vue({
+  el: '#vue-instagram',
+
+  components: {
+    'instagram-photo': instagramPhoto
+  },
+
+  data: {
+    photos: null
+  },
+
+  computed: {
+    basePhotos: function () {
+      return this.photos.slice(0, 10)
+    },
+    restPhotos: function () {
+      return this.photos.slice(10)
+    }
+  },
+
+  created: function () {
+    setInterval(function(){
+      if(!this.photos) return false
+
+      var randomBaseId = Math.floor(Math.random()*this.basePhotos.length);
+      var randomRestId = Math.floor(Math.random()*this.restPhotos.length);
+
+      var tmpBasePhoto = this.basePhotos[randomBaseId];
+
+      this.basePhotos.$set(randomBaseId, this.restPhotos[randomRestId]);
+      this.restPhotos.$set(randomRestId, tmpBasePhoto);
+    }.bind(this), 5000);
+  }
+});
+
+function flickrPhotoUrl(photo, size) {
+  return "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_"+size+".jpg";
+}
+
+function jsonFlickrApi(response) {
+  photos = response.photos.photo;
+  urls = [];
+  for (var i = 0; i < photos.length; i++) {
+    urls.push(flickrPhotoUrl(photos[i], "z"));
+  }
+  instagram.photos = urls;
+}
+
+// Fetch fotos
+$.get("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&user_id=140453763@N07&api_key=f0b84fba1c00631410b85b90720f52ba&format=json");
